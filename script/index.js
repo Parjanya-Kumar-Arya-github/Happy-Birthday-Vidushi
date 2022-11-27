84ec4d93364895253cfb5374652a712083a9e009
Parjanya Kumar Arya <Parjanya789@gmail.com>
2022-11-27 09:43:38 +0530

    commit

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-    birthdate: 'November 28, 2021',
+    birthdate: 'November 28, 2022',
     name: 'VIDUSHI'
 };
 
